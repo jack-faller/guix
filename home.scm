2acd29c28c4d26b258ee2f58eac7f9b2f103b755
Jack Faller <jack.t.faller@gmail.com>
2022-06-10 17:30:02 +0100

    Does stuff

--- home.scm
+++ home.scm
@@ -1,9 +1,32 @@
-(use-modules (ice-9 textual-ports))
+(use-modules (ice-9 textual-ports)
+             (gnu home)
+             (gnu home services)
+             (gnu home services shells)
+             (gnu services)
+             (gnu packages git)
+             (gnu packages vim)
+			 (guix gexp))
 
 (define wants-list
   (let ((computer (call-with-input-file "/etc/hostname" get-line)))
 	(cond
 	 ((equal? computer "mikhail") '(bash))
-	 ((equal? computer "sergey") '(zsh))
-	 ((equal? computer "birtha") '(zsh)))))
-(define (wants? x) (memq x wants-list))
+	 ((equal? computer "sergey") '(zsh sway))
+	 ((equal? computer "birtha") '(zsh bspwm)))))
+(define (wants? x)
+  (lambda (u) (if (memq x wants-list) u #f)))
+(define (list-when . args) (filter identity args))
+
+(home-environment
+ (packages (list-when
+			git
+			vim
+			((wants? 'bash) bash)))
+ (services
+  (list-when
+   ((wants? 'bash)
+	(service home-bash-service-type
+            (home-bash-configuration
+             (guix-defaults? #t)
+             (bash-profile '("export HISTFILE=$XDG_CACHE_HOME/.bash_history"))
+			 (bashrc (local-file ".bashrc"))))))))
--- make-server.sh
+++ make-server.sh
@@ -1,13 +1,17 @@
-ehco enter host name
+echo enter host name
 read -r NAME
 echo "$NAME" > /etc/hostname 
 echo "127.0.0.1    localhost
 ::1    localhost
-127.0.1.1    $NAME.localdomain    $NAME" >> /etc/hosts
+127.0.1.1    $NAME.localdomain    $NAME
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters" > /etc/hosts
 mkdir .ssh
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJNcDBG1O8GhsxaNwI3n76ECitK5Yt+Dqwd046Xgp6o jack.t.faller@gmail.com' >> .ssh/authorized_keys
 
-# pacman -S base-devel git
 useradd -m jack
 passwd jack
 echo 'jack   ALL=(ALL:ALL) ALL' >> /etc/sudoers
@@ -15,25 +19,26 @@ su jack
 cd ~
 mkdir .ssh
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJNcDBG1O8GhsxaNwI3n76ECitK5Yt+Dqwd046Xgp6o jack.t.faller@gmail.com' > .ssh/authorized_keys
-# git clone https://aur.archlinux.org/guix-installer.git
-# cd guix-installer
-# makepkg -sri
-# cd ~
-# rm -rf guix-installer
-
-cd ~
-mkdir .config
-git clone https://github.com/jack-faller/guix .config/guix
 
 cd /tmp
 wget https://git.savannah.gnu.org/cgit/guix.git/plain/etc/guix-install.sh
 chmod +x guix-install.sh
-./guix-install.sh
+sudo ./guix-install.sh
 
 cd ~
-sudo sed -i "s#ExecStart.*guix-daemon#& --substitute-urls='https://ci.guix.gnu.org https://bordeaux.guix.gnu.org https://substitutes.nonguix.org'#" /etc/systemd/system/guix-daemon.service
-sudo systemctl daemon-reload
-sudo systemctl restart guix-daemon.service
+guix install git
+GUIX_PROFILE="$HOME/.guix-profile"
+. "$GUIX_PROFILE/etc/profile"
+git config --global http.sslverify false
+git clone https://github.com/jack-faller/guix.git
+ln -s ~/guix/channels.scm ~/.config/guix/channels.scm
+
+cd ~
+sudo sed -i "s#ExecStart.*guix-daemon#& --substitute-urls='https://ci.guix.gnu.org https://substitutes.nonguix.org'#" /etc/systemd/system/guix-daemon.service
 curl https://substitutes.nonguix.org/signing-key.pub | sudo guix archive --authorize
 sudo guix archive --authorize < "$(find / -name ci.guix.gnu.org.pub | sed 1q)"
-sudo guix archive --authorize < "$(find / -name bordeaux.guix.gnu.org.pub | sed 1q)"
+sudo -i guix pull
+sudo systemctl daemon-reload
+sudo systemctl restart guix-daemon.service
+
+guix home reconfigure ~/guix/home.scm
