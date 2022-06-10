a645e5a7406b37d412ae0b278f2b567dbe20e2dc
Jack Faller <jack.t.faller@gmail.com>
2022-06-10 19:19:46 +0100

    Try to add shepherd service

--- home.scm
+++ home.scm
@@ -1,6 +1,7 @@
 (use-modules (ice-9 textual-ports)
              (guix gexp)
              (gnu services)
+             (gnu services web)
              (gnu home)
              (gnu home services)
              (gnu home services shells)
@@ -30,14 +31,22 @@
 			 #:locales (list "en_GB" "en_US")
 			 #:name "glibc-my-utf8-locales")
 			((wants? 'bash) bash)))
+
  (services
   (list-when
    ((wants? 'bash)
 	(service home-bash-service-type
-            (home-bash-configuration
-             (guix-defaults? #t)
-             (environment-variables '(("HISTFILE" . "$XDG_CACHE_HOME/.bash_history")
-									  ("GUIX_LOCPATH" . "$HOME/.guix-home/profile/lib/locale")))
-			 (bashrc `(,(local-file "bashrc")))
-			 (aliases `(("update-home" . ,(string-append "guix home reconfigure " (canonicalize-path "home.scm")))
-						("update-guix" . "sudo -i guix pull; guix gc -d 6m -C; systemctl restart guix-daemon.service")))))))))
+			 (home-bash-configuration
+			  (guix-defaults? #t)
+			  (environment-variables '(("HISTFILE" . "$XDG_CACHE_HOME/.bash_history")
+									   ("GUIX_LOCPATH" . "$HOME/.guix-home/profile/lib/locale")))
+			  (bashrc `(,(local-file "bashrc")))
+			  (aliases `(("update-home" . ,(string-append "guix home reconfigure " (canonicalize-path "home.scm")))
+						 ("update-guix" . "sudo -i guix pull; guix gc -d 6m -C; systemctl restart guix-daemon.service"))))))
+   ((wants? 'server)
+	(service nginx-service-type
+			 (nginx-configuration
+			  (server-blocks
+			   (list (nginx-server-configuration
+					  (server-name '("www.example.com"))
+					  (root "/srv/http/www.example.com"))))))))))
