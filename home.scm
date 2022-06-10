55dae8e6f9d23260ada65b6a988e15ef7624e4af
Jack Faller <jack.t.faller@gmail.com>
2022-06-10 19:04:08 +0100

    Adds update-guix alias

--- home.scm
+++ home.scm
@@ -39,4 +39,5 @@
              (environment-variables '(("HISTFILE" . "$XDG_CACHE_HOME/.bash_history")
 									  ("GUIX_LOCPATH" . "$HOME/.guix-home/profile/lib/locale")))
 			 (bashrc `(,(local-file "bashrc")))
-			 (aliases '(("update-guix" . "sudo -i guix pull; guix gc -d 6m -C; systemctl restart guix-daemon.service")))))))))
+			 (aliases '(("update-home" . (string-append "guix home reconfigure " (canonicalize-path "home.scm")))
+						("update-guix" . "sudo -i guix pull; guix gc -d 6m -C; systemctl restart guix-daemon.service")))))))))
