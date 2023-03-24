d7cce5d9abf5ad20b5b708496c184f9c29d5d8d3
Jack Faller <jack.t.faller@gmail.com>
2023-03-24 15:06:27 +0000

    Fixes adds linker to fix errors while compiling multiple files

--- dev/c.scm
+++ dev/c.scm
@@ -1,8 +1,10 @@
 (use-modules (gnu packages gcc)
+			 (gnu packages commencement)
 			 (gnu packages base)
 			 (gnu packages llvm)
 			 (gnu packages debug)
 			 (gnu packages valgrind)
 			 (gnu packages autotools)
 			 (gnu packages build-tools))
-(list gcc (list glibc "debug") binutils clang automake autoconf bear valgrind gnu-make cgdb); clang is for clangd
+(list gcc-toolchain (list glibc "debug") binutils
+	  clang automake autoconf bear valgrind gnu-make cgdb); clang is for clangd
