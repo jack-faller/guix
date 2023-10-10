46e836920caf6777f12bcbf30284cda913c18102
Jack Faller <jack.t.faller@gmail.com>
2023-10-10 19:13:54 +0100

    Fixes issue with printing in unsee.sh

--- files/qutebrowser/userscripts/unsee.sh
+++ files/qutebrowser/userscripts/unsee.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if echo "$1" | grep 'unsee.*cc/album#.\{12\}'; then
-   echo "$1" | sed 's/\(unsee\).*\(cc\/album#.\{12\}\)/open '"$2"' \1.\2/' >> "$QUTE_FIFO"
+if MATCH=$(echo "$1" | grep -m 1 -o 'unsee.*cc/album#.\{12\}'); then
+   echo "$MATCH" | sed 's/.*\(unsee\).*\(cc\/album#.\{12\}\).*/open '"$2"' \1.\2/' >> "$QUTE_FIFO"
 else
 	echo "message-error 'Could not determine unsee format.'" >> "$QUTE_FIFO"
 fi
