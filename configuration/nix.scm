35778ae80566ca73b98fcfdda04aac74d9989e0f
Jack Faller <jack.t.faller@gmail.com>
2023-12-21 22:29:37 +0000

    Removes unneeded form

--- configuration/nix.scm
+++ configuration/nix.scm
@@ -27,33 +27,31 @@
 	 (service-extension
 	  home-activation-service-type
 	  (λ (packages)
-		(with-imported-modules
-		 '((ice-9 popen) (ice-9 rdelim) (ice-9 textual-ports))
-		 #~(begin
-			 (use-modules (ice-9 popen) (ice-9 rdelim) (ice-9 textual-ports))
-			 (define packages '#$(sort packages string<))
-			 (define package-file
-			   (string-append (getenv "HOME") "/.cache/installed-nix-packages"))
-			 (define (invoke . args)
-			   (unless (= 0 (apply system* args))
-				 (error "failed to run nix command" args)))
-			 (invoke #$(executable-shell-script
-						"nix-add-channels"
-						"if [ \"$(nix-channel --list)\" = \"\" ]; then"
-						"  nix-channel --add https://nixos.org/channels/nixpkgs-unstable || exit 1"
-						"  nix-channel --update || exit 1"
-						"  nix-env -iA nixpkgs.nix nixpkgs.cacert || exit 1"
-						"  echo '()' > ~/.cache/installed-nix-packages"
-						"fi"))
-			 (setenv "NIXPKGS_ALLOW_UNFREE" "1")
-			 (unless (equal? packages (with-input-from-file package-file read))
-			   (display "installing nix packages") (newline)
-			   (if (null? packages)
-				   (invoke "nix-env" "--uninstall" ".*")
-				   (apply invoke "nix-env" "--remove-all" "--install"
-						  packages))
-			   (with-output-to-file package-file
-				 (lambda () (write packages) (newline))))))))))))
+		#~(begin
+			(use-modules (ice-9 popen) (ice-9 rdelim) (ice-9 textual-ports))
+			(define packages '#$(sort packages string<))
+			(define package-file
+			  (string-append (getenv "HOME") "/.cache/installed-nix-packages"))
+			(define (invoke . args)
+			  (unless (= 0 (apply system* args))
+				(error "failed to run nix command" args)))
+			(invoke #$(executable-shell-script
+					   "nix-add-channels"
+					   "if [ \"$(nix-channel --list)\" = \"\" ]; then"
+					   "  nix-channel --add https://nixos.org/channels/nixpkgs-unstable || exit 1"
+					   "  nix-channel --update || exit 1"
+					   "  nix-env -iA nixpkgs.nix nixpkgs.cacert || exit 1"
+					   "  echo '()' > ~/.cache/installed-nix-packages"
+					   "fi"))
+			(setenv "NIXPKGS_ALLOW_UNFREE" "1")
+			(unless (equal? packages (with-input-from-file package-file read))
+			  (display "installing nix packages") (newline)
+			  (if (null? packages)
+				  (invoke "nix-env" "--uninstall" ".*")
+				  (apply invoke "nix-env" "--remove-all" "--install"
+						 packages))
+			  (with-output-to-file package-file
+				(lambda () (write packages) (newline)))))))))))
 
 (define nix-system-services
   (list
