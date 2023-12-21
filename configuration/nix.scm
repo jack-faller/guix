e98dfc48310f3df160614f1f03fbd43cb21c44df
Jack Faller <jack.t.faller@gmail.com>
2023-12-21 22:41:49 +0000

    Simplifies logic in nix packages service

--- configuration/nix.scm
+++ configuration/nix.scm
@@ -28,11 +28,11 @@
 	  home-activation-service-type
 	  (λ (packages)
 		#~(begin
-			(use-modules (ice-9 popen) (ice-9 rdelim) (ice-9 textual-ports))
 			(define packages '#$packages)
 			(define (invoke . args)
 			  (unless (= 0 (apply system* args))
 				(error "failed to run nix command" args)))
+			(setenv "NIXPKGS_ALLOW_UNFREE" "1")
 			(invoke #$(executable-shell-script
 					   "nix-add-channels"
 					   "if [ \"$(nix-channel --list)\" = \"\" ]; then"
@@ -40,13 +40,10 @@
 					   "  nix-channel --update || exit 1"
 					   "  nix-env -iA nixpkgs.nix nixpkgs.cacert || exit 1"
 					   "fi"))
-			(setenv "NIXPKGS_ALLOW_UNFREE" "1")
-			(unless (= 0 (apply system* "nix-env" "--query" "--installed" packages))
-			  (display "installing nix packages") (newline)
-			  (if (null? packages)
-				  (invoke "nix-env" "--uninstall" ".*")
-				  (apply invoke "nix-env" "--remove-all" "--install"
-						 packages))))))))))
+			(unless (or (null? packages)
+						(= 0 (apply system* "nix-env" "--query" "--installed"
+									packages)))
+			  (apply invoke "nix-env" "--remove-all" "--install" packages)))))))))
 
 (define nix-system-services
   (list
