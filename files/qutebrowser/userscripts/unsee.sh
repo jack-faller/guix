c9d8496d9e3b37f175b9da117959fd20cc84ba9c
Jack Faller <jack.t.faller@gmail.com>
2024-01-06 12:46:21 +0000

    Simplify unsee.sh

--- files/qutebrowser/userscripts/unsee.sh
+++ files/qutebrowser/userscripts/unsee.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if MATCH=$(echo "$1" | grep -m 1 -o 'album#.\{12\}'); then
-   echo "$MATCH" | sed 's/.*album#\(.\{12\}\).*/open '"$2"' unsee.cc\/album#\1/' >> "$QUTE_FIFO"
+if MATCH=$(echo "$1" | tr -d '[:space:]' | grep -m 1 -o '#.\{12\}'); then
+   echo "$MATCH" | sed 's/.*/open '"$2"' unsee.cc\/album&/' >> "$QUTE_FIFO"
 else
 	echo "message-error 'Could not determine unsee format.'" >> "$QUTE_FIFO"
 fi
