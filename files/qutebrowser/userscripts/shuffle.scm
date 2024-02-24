9b0f8ab32275331d0c7766841bbd8d120af1ad5d
Jack Faller <jack.t.faller@gmail.com>
2024-02-24 00:50:37 +0000

    Adds missing file

---
+++ files/qutebrowser/userscripts/shuffle.scm
@@ -0,0 +1,23 @@
+#!/usr/bin/env guile
+!#
+
+(define url (getenv "QUTE_URL"))
+
+(set! *random-state* (random-state-from-platform))
+;; browser crashes unless you submit all commands at once with ";;"s
+(define cmd
+  (string-join
+   (append
+	(apply append
+		   (map
+			(λ (i)
+			  (define j (+ i 1))
+			  (define rand (random j))
+			  (list
+			   (string-append "tab-select " (number->string (+ rand 1)))
+			   "tab-move end"))
+			(reverse (iota (string->number (getenv "QUTE_TAB_INDEX"))))))
+	(list (string-append "tab-select " url)))
+   ";; "))
+
+(with-output-to-file (getenv "QUTE_FIFO") (λ () (display cmd)))
