8e43f7f8c515d131968d4fa83aada2553a46d24b
Jack Faller <jack.t.faller@gmail.com>
2024-03-30 14:06:48 +0000

    Uses package specifications in dev/c

--- dev/c.scm
+++ dev/c.scm
@@ -1,11 +1,5 @@
-(use-modules (gnu packages gcc)
-			 (gnu packages gdb)
-			 (gnu packages base)
-			 (gnu packages llvm)
-			 (gnu packages debug)
-			 (gnu packages valgrind)
-			 (gnu packages autotools)
-			 (gnu packages build-tools)
-			 (gnu packages commencement))
-(list gcc-toolchain (list glibc "debug") binutils
-	  clang automake autoconf bear valgrind gnu-make gdb cgdb); clang is for clangd
+(load "../file-utils.scm")
+(use-modules (file-utils))
+(specifications->package-list
+ "gcc-toolchain" "glibc:debug" "binutils" "clang" "automake" "autoconf" "bear"
+ "valgrind" "make" "gdb" "cgdb")
