d311896e7c0596a9c293c033b3e8ccee513b663d
Jack Faller <jack.t.faller@gmail.com>
2024-04-25 22:13:19 +0100

    Adds wayland in qt qpa platform

--- sergey-jack.scm
+++ sergey-jack.scm
@@ -34,6 +34,10 @@
 			 (videos "$HOME/vids")
 			 (pictures "$HOME/pics")
 			 (music "$HOME/music")))
+   (simple-service
+	'my-env-vars
+	home-environment-variables-service-type
+	'(("QT_QPA_PLATFORM" . "wayland;xcb")))
    (service
 	home-xdg-mime-applications-service-type
 	(home-xdg-mime-applications-configuration
