4b4a36a7a40e1e1122e6a1f70b763c8eb3cfe728
Jack Faller <jack.t.faller@gmail.com>
2024-05-07 15:36:29 +0100

    Adds ds and iip

---
+++ dev/ds.scm
@@ -0,0 +1,3 @@
+(load "../utilities.scm")
+(use-modules (utilities))
+(specifications->package-list "containerd" "docker" "docker-cli" "docker-compose" "openjdk:jdk")
---
+++ dev/iip.scm
@@ -0,0 +1,2 @@
+(use-modules (gnu packages))
+(map specification->package '("python" "python-lsp-server" "opencv" "python-numpy" "python-matplotlib"))
