8391d19cb7c8126ce3a47b7427186cd9fe69dc5a
Jack Faller <jack.t.faller@gmail.com>
2024-06-16 09:32:51 +0100

    Adds missing rofi files

---
+++ dotfiles/jack/.config/rofi/unicode-script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+script='{
+    desc = $11 == "" ? $2 : $11
+    if (desc !~ "^<") {
+        if ($3 !~ "^[MZC]")
+	    printf "%c %s %s", strtonum("0x" $1), desc, $1
+	else
+	    printf "%s %s", desc, $1
+	printf "\x00info\x1f%s\n", $1
+    }
+}'
+if [ "$ROFI_RETV" = 0 ]; then
+    awk -v FS=';' "$script" "$UNICODE_DATA_TXT"
+elif [ "$ROFI_RETV" = 1 ]; then
+    nohup sh -c "wait $(pgrep rofi); keyboard-input $(printf \u$ROFI_INFO)" 0<&- &>/dev/null &
+fi
