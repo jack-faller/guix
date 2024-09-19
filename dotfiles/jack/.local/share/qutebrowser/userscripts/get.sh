16487fcffa5f8f62ad9e6ce1dd1dafb59cada3be
Jack Faller <jack.t.faller@gmail.com>
2024-09-19 14:31:35 +0100

    Adds missing quotes to get.sh

--- dotfiles/jack/.local/share/qutebrowser/userscripts/get.sh
+++ dotfiles/jack/.local/share/qutebrowser/userscripts/get.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-line=$(cat "$QUTE_CONFIG_DIR/stowed" | sort | dmenu)
-url=$(echo "$line" | sed 's/.* //g') #strip tab title
+line="$(cat "$QUTE_CONFIG_DIR/stowed" | sort | dmenu)"
+url="$(echo "$line" | sed 's/.* //g')" #strip tab title
 
-if [ $(grep -x -v "$line" "$QUTE_CONFIG_DIR/stowed") = "" ]; then #don't open tabs not in file
+if [ "$(grep -x -v "$line" "$QUTE_CONFIG_DIR/stowed")" = "" ]; then #don't open tabs not in file
     exit
 fi
 
