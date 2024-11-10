8eba59d631cced17b43abf3e9454e9efa2c47b6f
Jack Faller <jack.t.faller@gmail.com>
2024-11-10 12:58:13 +0000

    Fixes incorrect exit without error in percent

--- files/percent.c
+++ files/percent.c
@@ -14,41 +14,43 @@ int hex(char c) {
 }
 
 int main(int argc, char **argv) {
-  if (argc == 3) {
-    if (0 == strcmp(argv[1], "encode")) {
-      for (char *c = argv[2]; *c != '\0'; ++c) {
-        if (BETWEEN(*c, '0', '9')
-            || BETWEEN(*c, 'A', 'Z')
-            || BETWEEN(*c, 'a', 'z')
-            || *c == '-'
-            || *c == '.'
-            || *c == '_'
-            || *c == '~')
-          printf("%c", (int)*c);
-        else
-          printf("%%%.2hhX", *c);
-      }
-    } else if (0 == strcmp(argv[1], "decode")) {
-      for (char *c = argv[2]; *c != '\0'; ++c) {
-        int a, b;
-        if (*c != '%')
-          printf("%c", (int)*c);
-        else if (c[1] == '\0' || c[2]=='\0') {
-          fprintf(stderr, "Not enough characters in hex sequence %.2s.\n", c);
-          return 1;
-        } else if (a = hex(c[1]), b = hex(c[2]), a == -1 || b == -1) {
-          fprintf(stderr, "Invalid hex sequence %.3s.\n", c);
-          return 1;
-        } else {
-          printf("%c", a * 16 + b);
-          c += 2;
-        }
-      }
-    } else {
-      fprintf(stderr, "Usage: percent [encode/decode] <string>.\n");
-      return 1;
+  if (argc != 3)
+    goto error;
+  if (0 == strcmp(argv[1], "encode")) {
+    for (char *c = argv[2]; *c != '\0'; ++c) {
+      if (BETWEEN(*c, '0', '9')
+          || BETWEEN(*c, 'A', 'Z')
+          || BETWEEN(*c, 'a', 'z')
+          || *c == '-'
+          || *c == '.'
+          || *c == '_'
+          || *c == '~')
+        printf("%c", (int)*c);
+      else
+        printf("%%%.2hhX", *c);
     }
+  } else if (0 == strcmp(argv[1], "decode")) {
+    for (char *c = argv[2]; *c != '\0'; ++c) {
+      int a, b;
+      if (*c != '%')
+        printf("%c", (int)*c);
+      else if (c[1] == '\0' || c[2]=='\0') {
+        fprintf(stderr, "Not enough characters in hex sequence %.2s.\n", c);
+        return 1;
+      } else if (a = hex(c[1]), b = hex(c[2]), a == -1 || b == -1) {
+        fprintf(stderr, "Invalid hex sequence %.3s.\n", c);
+        return 1;
+      } else {
+        printf("%c", a * 16 + b);
+        c += 2;
+      }
+    }
+  } else {
+    goto error;
   }
   printf("\n");
   return 0;
+ error:
+  fprintf(stderr, "Usage: percent [encode/decode] <string>.\n");
+  return 1;
 }
