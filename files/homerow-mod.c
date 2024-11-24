849dfb6e38fa3b07d2291166e12e9dd68c549b6e
Jack Faller <jack.t.faller@gmail.com>
2024-11-24 00:17:46 +0000

    Adds space as raise modifier

--- files/homerow-mod.c
+++ files/homerow-mod.c
@@ -1,3 +1,4 @@
+#include <linux/input-event-codes.h>
 #include <linux/input.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -15,6 +16,8 @@ typedef struct node {
 	input_event event;
 	struct node *next;
 } node;
+// Reuse a key not present on most keyboards for raise.
+#define KEY_RAISE KEY_KATAKANAHIRAGANA
 node key_status[KEY_MAX];
 code real_code(node *n) { return n - key_status; }
 // Note: latest_node may point to junk when oldest_node is NULL.
@@ -34,7 +37,41 @@ void write_event(input_event *event) {
 		);
 #endif
 #ifndef DEBUG
+	static value raise_active = UP;
+	if (event->code == KEY_RAISE)
+		raise_active = event->value;
+	code old = event->code;
+	if (raise_active == UP) {
+		switch (event->code) {
+		case KEY_LEFTSHIFT: event->code = KEY_CAPSLOCK; break;
+		case KEY_CAPSLOCK: event->code = KEY_ESC; break;
+		}
+	} else {
+		switch (event->code) {
+		case KEY_Q: event->code = KEY_F1; break;
+		case KEY_W: event->code = KEY_F2; break;
+		case KEY_E: event->code = KEY_F3; break;
+		case KEY_R: event->code = KEY_F4; break;
+		case KEY_T: event->code = KEY_F5; break;
+		case KEY_Y: event->code = KEY_F6; break;
+		case KEY_U: event->code = KEY_F7; break;
+		case KEY_I: event->code = KEY_F8; break;
+		case KEY_O: event->code = KEY_F9; break;
+		case KEY_P: event->code = KEY_F10; break;
+		case KEY_A: event->code = KEY_1; break;
+		case KEY_S: event->code = KEY_2; break;
+		case KEY_D: event->code = KEY_3; break;
+		case KEY_F: event->code = KEY_4; break;
+		case KEY_G: event->code = KEY_5; break;
+		case KEY_H: event->code = KEY_6; break;
+		case KEY_J: event->code = KEY_7; break;
+		case KEY_K: event->code = KEY_8; break;
+		case KEY_L: event->code = KEY_9; break;
+    case KEY_SEMICOLON: event->code = KEY_0; break;
+		}
+	}
 	fwrite(event, sizeof(*event), 1, stdout);
+	event->code = old;
 #endif
 }
 
@@ -48,6 +85,7 @@ code translate_code(code code) {
 	case KEY_K: return KEY_RIGHTCTRL;
 	case KEY_L: return KEY_RIGHTMETA;
 	case KEY_SEMICOLON: return KEY_LEFTALT; // Use left alt to avoid AltGr.
+	case KEY_SPACE: return KEY_RAISE;
 	default: return code;
 	}
 }
@@ -104,11 +142,6 @@ void handle_event(input_event *input) {
 		key_status[input->code].event = *input;
 		write_event(input);
 		return;
-	}
-
-	switch (input->code) {
-	case KEY_LEFTSHIFT: input->code = KEY_CAPSLOCK; break;
-	case KEY_CAPSLOCK: input->code = KEY_ESC; break;
 	}
 
 retry_event:
