2b137b6d732f282ef96d54cb040122f05e68bc76
Jack Faller <jack.t.faller@gmail.com>
2025-03-30 14:59:02 +0100

    Fixes failure to error in populate-blocklists.sh

--- files/populate-blocklists.sh
+++ files/populate-blocklists.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-set -eo pipefail
+set -euo pipefail
 for i in 'www.youtube.com'; do
-	 nft add element inet filter ip4blocklist \{ "$(dig "$i" a +noall +answer | cut -f4 | sed 1d | tr '\n' ', ')" \}
+	IPs="$(dig "$i" a +noall +answer | cut -f4 | sed 1d | tr '\n' ', ')"
+	nft add element inet filter ip4blocklist \{ "$IPs" \}
 done
