b69bd9aa996cb3d14f692276ef3a9a4534c4f5d2
Jack Faller <jack.t.faller@gmail.com>
2025-05-21 11:50:48 +0100

    Reads blocklists from file

--- files/populate-blocklists.sh
+++ files/populate-blocklists.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 # Wait for network to come up.
 while ! ping -c 1 -W 10 google.com ; do sleep 5; done
-for i in 'www.youtube.com'; do
-	IPs="$(dig "$i" a -4 +noall +answer | cut -f4 | sed 1d | tr '\n' ', ')"
+nft flush set inet filter ip4blocklist
+while read i; do
+	IPs="$(dig "$i" a -4 +noall +answer | grep -Eo '[0-9]+.[0-9]+.[0-9]+.[0-9]+$' | tr '\n' ', ')"
 	nft add element inet filter ip4blocklist \{ "$IPs" \}
-done
+done < /blocked-ips.txt
