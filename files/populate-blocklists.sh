d763160aa5b0629638803062c53fd9d43bb99867
Jack Faller <jack.t.faller@gmail.com>
2025-08-26 14:32:57 +0100

    Adds comments to blocklists file

--- files/populate-blocklists.sh
+++ files/populate-blocklists.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 # Wait for network to come up.
 while ! ping -c 1 -W 10 google.com ; do sleep 5; done
 nft flush set inet filter ip4blocklist
-while read i; do
+grep -v '^[\t ]*#' | while read i; do
 	IPs="$(dig "$i" a -4 +noall +answer | grep -Eo '[0-9]+.[0-9]+.[0-9]+.[0-9]+$' | tr '\n' ', ')"
 	nft add element inet filter ip4blocklist \{ "$IPs" \}
 done < /blocked-ips.txt
