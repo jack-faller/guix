db4c2dc73631698eefbe976cc0da2140e4a14d49
Jack Faller <jack.t.faller@gmail.com>
2026-03-24 10:29:09 +0000

    Bumps Discord version

--- packages/discord.scm
+++ packages/discord.scm
@@ -69,14 +69,14 @@
 (define-public discord
   (package
    (name "discord")
-   (version "0.0.128")
+   (version "0.0.130")
    (source
     (origin
      (method url-fetch)
      (uri (string-append "https://cdn.discordapp.com/apps/linux/" version
                          "/discord-" version ".tar.gz"))
      (sha256
-      (base32 "01f1hiqqhysva9qfgxvwprfs86pkisbilm0xbs60ikz48rfycigp"))))
+      (base32 "174zycdhi9qs93flwdl37n6aj866l07j48lzmaf754wg4py0rhhl"))))
    ;; Use this build system to set XDG_DATA_DIRS and other variables.
    (build-system chromium-binary-build-system)
    (arguments
