c3fcd88483bfff9117790ef662391796a81997a8
Jack Faller <jack.t.faller@gmail.com>
2026-05-06 18:36:17 +0100

    Bumps Discord version

---
+++ packages/discord-info.scm
@@ -0,0 +1,8 @@
+
+(define discord-version "1.0.137")
+(define discord-hash "1814gnjsag27gif160pw5yyd588i8395wh7krq9g57i5whd08m26")
+(define discord-uri "https://stable.dl2.discordapp.net/distro/app/stable/linux/x64/1.0.137/full.distro")
+(define module-uris '("https://stable.dl2.discordapp.net/distro/app/stable/linux/x64/1.0.137/discord_desktop_core/1/full.distro" "https://stable.dl2.discordapp.net/distro/app/stable/linux/x64/1.0.137/discord_erlpack/1/full.distro" "https://stable.dl2.discordapp.net/distro/app/stable/linux/x64/1.0.137/discord_spellcheck/1/full.distro" "https://stable.dl2.discordapp.net/distro/app/stable/linux/x64/1.0.137/discord_utils/1/full.distro" "https://stable.dl2.discordapp.net/distro/app/stable/linux/x64/1.0.137/discord_voice/1/full.distro"))
+(define module-hashes '("03ilypmb2b35im43b7zf2q4p8nx29abw1ph31ga6avbfz1r051zn" "0m2p4chrqyz1icz4kgfzqszhla7kq958vafsh2pry6x7blyps94m" "0zkmm5dlsw5bvcj74lm72jzlm3wxjg34kb9y1drmlhsmd863qmmp" "09bhd9csgraykp9k8ng5fb9f2bvddbhjc0bl9j2fabryhdphaz8l" "0i3ayxrx39w1nnji7cyyg38jq2825qnna9bd5hw8ywnxwmgbg6sz"))
+(define module-names '("discord_desktop_core" "discord_erlpack" "discord_spellcheck" "discord_utils" "discord_voice"))
+
